4123d77145b3c5b49a5025e9f2e1365fda31d798
Thiago-stack <57415873+Thiago-stack@users.noreply.github.com>
2019-11-05 21:27:53 -0500

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,18 @@
+
+public class Circle
+{
+   private int radius;
+   public void setRadius(int r)
+    {
+      radius = r;
+    }
+   public int getRadius()
+    {
+      return radius;
+    }
+   public int getDiameter()
+    {
+      return radius * 2;
+    }
+      
+}
